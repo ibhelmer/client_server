01b6b84668c15c4027146320d3370c4d66331536
Ib Helmer Nielsen <ihn@ihn.dk>
2020-11-09 11:45:17 +0100

    New Project

--- client.py
+++ client.py
@@ -1,15 +1,18 @@
 # Client.py
-# Simple one-way chat, client recive message from server !!
-# Ib Helmer Nielsen, UCN october 2020
+# Simple Client that send text ("Hello Server") string to server
+# Ib Helmer Nielsen, UCN november 2020
 
-host = "127.0.0.1"  # Set to address of server
-port = 7913  # Set to port used by server
+host = "127.0.0.1"                  # Set to address of server
+port = 7913                         # Set to port used by server
 
-from socket import *  # Imports socket module
+from socket import *                # Imports socket module
 
-s = socket(AF_INET, SOCK_STREAM)  # Creates a socket
-s.connect((host, port))  # Connect to server address
-
-msg = s.recv(1024)  # Receives data upto 1024 bytes and stores in variables msg
-print("Message from server : " + msg.strip().decode('ascii'))
+s = socket(AF_INET, SOCK_STREAM)    # Creates a socket
+print("Connection to server...")
+s.connect((host, port))             # Connect to server address
+print("Send msg Hello server!!")
+data = "Hello server!!"
+s.send(data.encode('utf-8'))        # Encode data to utf-8 and sends data to server
+msg = s.recv(1024)                  # Receives upto 1024 bytes from server and stores it in variables msg
+print("Back from server : " + msg.strip().decode('ascii'))
 s.close()  # Closes the socket and end the program
--- server.py
+++ server.py
@@ -1,18 +1,20 @@
 # server.py
-# Simple one-way chat, server send message to client !!
-# Ib Helmer Nielsen, UCN october 2020
+# Simple server, that recieves message from client and send message back
+# Ib Helmer Nielsen, UCN november 2020
 
-host = "127.0.0.1"  # Set the server address to your PC's IP address
-port = 7913  # Sets port to 7913 ( > 1023)
-from socket import *  # Imports socket module
+host = "127.0.0.1"          # Set the server address to your PC's IP address
+port = 7913                 # Sets port to 7913 ( > 1023)
+from socket import *        # Imports socket module
 
 s = socket(AF_INET, SOCK_STREAM)
-s.bind((host, port))  # Binds the socket. Note that the input to
-s.listen(1)  # Sets socket to listening state (simple chat app, only 1 connection !!)
+s.bind((host, port))        # Binds the socket. Note that the input to
+s.listen(1)                 # Sets socket to listening state (simple chat app, only 1 connection !!)
 print("Listening for connections.. ")
-q, addr = s.accept()  # Accepts incoming request from client and returns
-# socket and address to variables q and addr
-print("Clinet from {0}".format(addr))  # H
-data = input("Enter data to be send:  ")  # Data to be send is stored in variable data from user
-q.send(data.encode('utf-8'))  # Encode data to utf-8 and sends data to client
-s.close()  # Close socket, end of program
+q, addr = s.accept()        # Accepts incoming request from client and returns
+                            # socket and address to variables q and addr
+print("Client from {0}".format(addr))  # Clint address
+msg = q.recv(1024)          # Receives upto 1024 bytes from client and stores it in variables msg
+print("Revieved from client : "+ msg.strip().decode('ascii'))
+data = "Hello Client !!"    # Data to be send is stored in variable data from user
+q.send(data.encode('utf-8'))# Encode data to utf-8 and sends data to client
+s.close()                   # Close socket, end of program
